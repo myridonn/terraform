04bd1a54173f9b2b1867e2b0d60551195e1e8ba6
swaranjali09 <151865392+swaranjali09@users.noreply.github.com>
2024-03-08 00:24:19 +0530

    Update provider.tf
    changes

--- provider.tf
+++ provider.tf
@@ -1,19 +1,5 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-# Configure the AWS Provider
-provider "aws" {
-  region = "ap-south-1"
-}
-
-terraform {
-  required_providers {
     docker = {
       source  = "kreuzwerker/docker"
       version = "2.16.0"
