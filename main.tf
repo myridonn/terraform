0a88f1e8fc7e259f0fccca5d3607b2724d942d68
miller <61946873+myridonn@users.noreply.github.com>
2024-10-30 01:32:20 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ resource "docker_image" "nginx" {
 }
 
 resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
-  name  = "nginx_container"
+  image = docker_image.nginx.image_id
+  name  = "nginx"
   ports {
     internal = 80
     external = 8080
