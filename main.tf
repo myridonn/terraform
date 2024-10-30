115490eaa720454abd90a43557966c6b946ffbc8
miller <61946873+myridonn@users.noreply.github.com>
2024-10-30 01:01:53 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "docker_image" "nginx" {
-  name         = "nginx:latest"
+  name         = "nginx:1.27"
   keep_locally = false
 }
 
 resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
+  image = docker_image.nginx.1.27
   name  = "tutorial"
   ports {
     internal = 80
